702397d6586b4565145716bf07296085c842caf0
cmerkatas <merxri@gmail.com>
2022-04-19 16:58:44 +0300

    ..

--- Rutils/tsadata.R
+++ Rutils/tsadata.R
@@ -6,3 +6,12 @@ data = geyser[60:210, 1]
 pacf(data)
 
 library(timeSeries)
+
+library(TSA)
+library(astsa)
+load("tsa3.rda")
+
+
+tsplot(jj, col=4, type="o", ylab="Quarterly Earnings per Share")
+tsplot(globtemp, col=4, type="o", ylab="Global Temperature Deviations")
+tsplot(gtemp_land, col=4, type="o", ylab="Global Temperature Deviations")
